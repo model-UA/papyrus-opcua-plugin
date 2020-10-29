aaa30fa905672842baf7ed2d9ddd24d46c6e96c5
Christoph Lehr <christoph@localhost.localdomain>
2020-10-29 22:16:51 +0100

    #15: added edge decorators

---
+++ at.ac.tuwien.auto.modelua.papyrus.opcua.diagram/src/at/ac/tuwien/auto/modelua/papyrus/opcua/diagram/decorations/HasComponentDecorator.java
@@ -0,0 +1,49 @@
+package at.ac.tuwien.auto.modelua.papyrus.opcua.diagram.decorations;
+
+import org.eclipse.draw2d.PolygonDecoration;
+import org.eclipse.draw2d.geometry.PointList;
+
+public class HasComponentDecorator extends PolygonDecoration  {
+	/** Default X scale value. */
+	private static final int DEFAULT_SCALE_X = 15;
+
+	/** Default Y scale value. */
+	private static final int DEFAULT_SCALE_Y = 5;
+	
+	public static final PointList TIP = new PointList();
+	
+	static {
+		TIP.addPoint(-1, 1);
+		TIP.addPoint(-1, -1);
+	}
+	
+	/**
+	 * Constructor.
+	 */
+	public HasComponentDecorator() {
+		init();
+	}
+
+	/**
+	 * Initialize the decoration.
+	 */
+	protected void init() {
+		setTemplate(TIP);
+		setScale(DEFAULT_SCALE_X, DEFAULT_SCALE_Y);
+	}
+
+	/**
+	 * <pre>
+	 * Overridden to take into account of the line with in the scale (better representation).
+	 * </pre>
+	 * 
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.draw2d.Polyline#setLineWidth(int)
+	 */
+	@Override
+	public void setLineWidth(final int w) {
+		setScale(DEFAULT_SCALE_X + w, DEFAULT_SCALE_Y + w);
+		super.setLineWidth(w);
+	}
+}

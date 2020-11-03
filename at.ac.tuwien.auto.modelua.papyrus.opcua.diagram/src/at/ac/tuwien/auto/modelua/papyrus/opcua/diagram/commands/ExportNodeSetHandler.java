fd460305d473dd82e5be5935f4bb44aa6138f727
Christoph Lehr <christoph@localhost.localdomain>
2020-11-03 09:39:12 +0100

    #6 added command and property tester configuration, added basic commands

---
+++ at.ac.tuwien.auto.modelua.papyrus.opcua.diagram/src/at/ac/tuwien/auto/modelua/papyrus/opcua/diagram/commands/ExportNodeSetHandler.java
@@ -0,0 +1,15 @@
+package at.ac.tuwien.auto.modelua.papyrus.opcua.diagram.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ExportNodeSetHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.out.println("Wuzz up");
+		return null;
+	}
+
+}
---
+++ at.ac.tuwien.auto.modelua.papyrus.opcua.diagram/src/at/ac/tuwien/auto/modelua/papyrus/opcua/diagram/commands/IsNodeSetModelPropertyTester.java
@@ -0,0 +1,42 @@
+package at.ac.tuwien.auto.modelua.papyrus.opcua.diagram.commands;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.papyrus.emf.facet.custom.metamodel.v0_2_0.internal.treeproxy.EObjectTreeElement;
+import org.eclipse.uml2.uml.Model;
+
+import at.ac.tuwien.auto.modelua.papyrus.opcua.diagram.OpcUaDiagramResources;
+
+public class IsNodeSetModelPropertyTester extends PropertyTester {
+	
+	private final static String EXPORTABLE_MODEL_PROPERTY = "isNodeSetModel"; //$NON-NLS-1$
+	private final static String DIAGRAM_VIEWPOINT = OpcUaDiagramResources.DIAGRAM_VIEWPOINT;
+	@Override
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+		boolean propertyTest = false;
+		if (EXPORTABLE_MODEL_PROPERTY.equals(property)) {
+			if (receiver instanceof IStructuredSelection) {
+				IStructuredSelection selection =(IStructuredSelection) receiver;
+				
+				if(selection.getFirstElement() instanceof EObjectTreeElement)
+				{
+					EObjectTreeElement treeElement = (EObjectTreeElement) selection.getFirstElement();
+					if(treeElement.getEObject() instanceof Model)
+					{
+						Model model = (Model) treeElement.getEObject();
+						String viewpoint = model.getViewpoint();
+						
+						if(viewpoint != null && viewpoint.equalsIgnoreCase(DIAGRAM_VIEWPOINT))
+						{
+							propertyTest = true;
+						}
+					}
+					
+				}
+				
+			}
+		}
+		return propertyTest;
+	}
+
+}

3fa1904a8ff736563710d2fadf39fa49c6cdfda8
Christoph Lehr <christoph@localhost.localdomain>
2020-11-08 12:22:17 +0100

    #31: added missing plugins/features

---
+++ at.ac.at.tuwien.modelua.papyrus.opcua.diagram.transformation/src/at/ac/at/tuwien/modelua/papyrus/opcua/diagram/transformation/OpcUaDiagramTransformationResources.java
@@ -0,0 +1,7 @@
+package at.ac.at.tuwien.modelua.papyrus.opcua.diagram.transformation;
+
+import at.ac.at.tuwien.modelua.papyrus.opcua.diagram.transformation.transformer.SynchHandler;
+
+public class OpcUaDiagramTransformationResources {
+	public static SynchHandler DIAGRAM_SYNCH_HANDLER = Activator.getSynchHandler();
+}
---
+++ at.ac.at.tuwien.modelua.papyrus.opcua.diagram.transformation/src/at/ac/at/tuwien/modelua/papyrus/opcua/diagram/transformation/transformer/UpdateUMLClassCommand.java
@@ -0,0 +1,37 @@
+package at.ac.at.tuwien.modelua.papyrus.opcua.diagram.transformation.transformer;
+
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.uml2.uml.Class;
+
+public class UpdateUMLClassCommand extends RecordingCommand {
+
+	private InstanceSyncHandler handler;
+	private Class obj;
+	private boolean success = false;
+	
+	public UpdateUMLClassCommand(TransactionalEditingDomain domain) {
+		super(domain);
+	}
+
+	public void registerInstanceSynchHandler(InstanceSyncHandler handler)
+	{
+		this.handler = handler;
+	}
+	
+	public void setClass(Class obj)
+	{
+		this.obj = obj;
+	}
+	
+	@Override
+	protected void doExecute() {
+		this.success = this.handler.transformClass(this.obj);
+	}
+	
+	public boolean getCommandResult()
+	{
+		return this.success;
+	}
+
+}
